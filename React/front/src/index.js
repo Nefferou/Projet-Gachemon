0e269f19a18b69550ad6ec5839c01eaec3c85539
Nefferou <fertijulien@gmail.com>
2023-03-13 17:11:21 +0100

    Test

--- React/front/src/index.js
+++ React/front/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Scss/index.scss';
+import './Sass/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
