a030f7789e5394acd87c4a9896869aab992c6538
Nefferou <fertijulien@gmail.com>
2023-03-13 16:54:13 +0100

    React Create Project

---
+++ React/front/src/App.js
@@ -0,0 +1,13 @@
+import './Scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
