aa358baea0f03883de31764f6fb8e263a7879168
Nefferou <fertijulien@gmail.com>
2023-03-14 15:18:06 +0100

    Test

--- React/front/src/index.js
+++ React/front/src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import reportWebVitals from './reportWebVitals';
-
-import './Scss/index.scss';
-
+import './SCSS/index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<App />} />
-        <Route path="/connect" element={<App />} />
-        <Route path={"/*"} element={<Navigate to="/home" />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
-reportWebVitals();
+
