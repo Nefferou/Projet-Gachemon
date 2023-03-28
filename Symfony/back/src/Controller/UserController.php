99acde140ac097dee91af0526dacfc9165a230df
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-03-28 10:52:06 +0200

    acces origin

--- Symfony/back/src/Controller/UserController.php
+++ Symfony/back/src/Controller/UserController.php
@@ -17,7 +17,7 @@ class UserController extends AbstractController
     public function actionConnection(EntityManagerInterface $entityManager): JsonResponse
     {           
         $request = Request::createFromGlobals();
-        if($parameters = json_decode($request->getContent(), true) != null){
+        $parameters = json_decode($request->getContent(), true);
         
         $repo = $entityManager->getRepository(User::class);
         $user = $repo->verifyAccount($parameters['username'],$parameters['password']);
@@ -27,12 +27,7 @@ class UserController extends AbstractController
             ], Response::HTTP_NOT_FOUND);
         }
 
-        return new JsonResponse($this->serializeUser($user), Response::HTTP_OK, ['accept' => 'json'], true);
-        }else{
-            $repo = $entityManager->getRepository(User::class);
-            $users = $repo->findAllUsers();
-            return new JsonResponse(json_encode($this->serializeArrayUser($users)), Response::HTTP_OK, ['accept' => 'json'], true);
-        }
+        return new JsonResponse(json_encode($this->serializeUser($user)), Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
     #[Route('/api/user', name: 'api_user_register',methods: ['Post'])]
@@ -41,25 +36,23 @@ class UserController extends AbstractController
         $request = Request::createFromGlobals();
         $content = $request->getContent();
 
-        // Decode the JSON string to an associative array
         $data = json_decode($content, true);
         $username = $data['username'];
         $password = $data['password'];
         $email = $data['email'];
         $repo = $entityManager->getRepository(User::class);
         $user = new User();
-        $id_profile = $repo->findLastIdPlayer();
+        $id_profile = $repo->findLastIdPlayer()[0]['id_profile'];
         return new JsonResponse($data,Response::HTTP_CREATED, ['accept' => 'json'], true);
-        $user->setUsername($request->get('username'))->setPassword($request->get('password'))->setEmail($request->get('email'))->setIdProfile($id_profile+1);
+        $user->setUsername($username)->setPassword($password)->setEmail($email)->setIdProfile($id_profile+1);
         
         if(is_null($user)){
             return new JsonResponse([
                 'error' => 'Not Acceptable'
             ], Response::HTTP_NOT_ACCEPTABLE);
         }
-        $repo->save($user);
-        $repo->flush();
-        return new JsonResponse($this->serializeUser($user),Response::HTTP_CREATED, ['accept' => 'json'], true);
+        $repo->save($user,true);
+        return new JsonResponse(json_encode($this->serializeUser($user)),Response::HTTP_CREATED, ['accept' => 'json'], true);
     }
 
     
@@ -71,13 +64,5 @@ class UserController extends AbstractController
             'email' => $user->getEmail(),
             'id_profile' => $user->getIdProfile()
         );
-    }
-    private function serializeArrayUser(Array $users)
-    {
-        $listUser = array();
-        foreach ($users as $user) {
-            array_push($listUser,json_encode($this->serializeUser($user)));
-        }
-        return $listUser;
     }
 }
