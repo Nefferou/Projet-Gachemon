e481efd8be898f6b354c891c628371f1d8bdf77a
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-04-03 11:50:37 +0200

    verify email exists

--- React/front/src/Page/Register.jsx
+++ React/front/src/Page/Register.jsx
@@ -14,32 +14,57 @@ function Register() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-        const navigate = useNavigate();
 
-    const handleRegister = () => {
-        const data = {
-            username: username,
-            email:email,
-            password: password
+    const [emailExists, setEmailExists] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const navigate = useNavigate();
+
+    const checkEmailExists = () => {
+        const jsonBody = {
+            email: email
         }
-
-        axios.post('https://gachemon.osc-fr1.scalingo.io/api/user',JSON.stringify(data),{
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
+        axios.get(`https://gachemon.osc-fr1.scalingo.io/api/user/email`, JSON.stringify(jsonBody))
             .then(response => {
-                navigate("/login");
+                if (response.data.length > 0) {
+                    setEmailExists(true);
+                    setErrorMessage("This email address is already registered.");
+                } else {
+                    setEmailExists(false);
+                    setErrorMessage(null);
+                }
             })
             .catch(error => console.error(error));
     }
+
+    const handleRegister = () => {
+        if (!emailExists) {
+            const data = {
+                username: username,
+                email:email,
+                password: password
+            }
+    
+            axios.post('https://gachemon.osc-fr1.scalingo.io/api/user',JSON.stringify(data),{
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    navigate("/login");
+                })
+                .catch(error => console.error(error));
+        }
+    }
+
     return (
         <div className="center">
         <Stack id="register" spacing={2}>
             <ThemeProvider theme={theme}>
                 <h1>Register</h1>
                 <TextField id="outlined-basic" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)}/>
-                <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) => setEmail(e.target.value)}/>
+                <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) => setEmail(e.target.value)} onBlur={checkEmailExists}/>
+                {errorMessage && <p className="error">{errorMessage}</p>}
                 <TextField id="outlined-basic" label="Password" variant="outlined" onChange={(e) => setPassword(e.target.value)} type="password" />
                 <TextField id="outlined-basic" label="Confirm password" variant="outlined" type="password" />
                 <Button variant="contained" onClick={handleRegister}>Create Account</Button>
