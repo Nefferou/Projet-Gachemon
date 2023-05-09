7bf6baeb821e3ab07e0a4e07c2cdaef9777e600a
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-05-09 14:27:24 +0200

    add token param in url

--- React/front/src/Composants/HeaderProfile.jsx
+++ React/front/src/Composants/HeaderProfile.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import { useNavigate  } from 'react-router-dom';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -8,23 +9,36 @@ import HomeIcon from '@mui/icons-material/Home';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function HeaderProfile() {
+  const token = useState(sessionStorage.getItem("token"));
+
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    const url = new URL('https://projet-angular-gachemon.vercel.app/boutique/');
+    url.searchParams.append('token', token[0]);
+    navigate(url.href);
+  };
+  
+
     return (
         <Box>
-          <AppBar position="static">
-            <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-              <IconButton href='./app' size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }}>
-                <HomeIcon />
-              </IconButton>
-              <Typography variant="h6" noWrap component="div" sx={{ display: { sm: 'block' } }}>
-                Profile
-              </Typography>
-              <Box sx={{ flexGrow: 0 }}>
-                <IconButton href='https://projet-angular-gachemon.vercel.app/boutique' size="large" edge="end" color="inherit" aria-label="open drawer" sx={{ ml: 2 }}>
-                  <ShoppingBasketIcon />
-                </IconButton>
-              </Box>
-            </Toolbar>
-          </AppBar>
+            <AppBar position="static">
+                <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+                    <IconButton href='./app' size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }}>
+                        <HomeIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div" sx={{ display: { sm: 'block' } }}>
+                        Profile
+                    </Typography>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <IconButton size="large" edge="end" color="inherit" aria-label="open drawer" sx={{ ml: 2 }}
+                               onClick={handleRedirect}
+                               >
+                            <ShoppingBasketIcon />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </Box>
     );
 }
